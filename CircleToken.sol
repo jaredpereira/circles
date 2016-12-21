91b7c2c0af150f9e99f2eb8d0f797c9875894a54
Jared Pereira <jared@otlw.co>
2016-12-21 16:33:35 -0500

    added the payout and inflation functions

--- CircleToken.sol
+++ CircleToken.sol
@@ -20,30 +20,32 @@ contract CircleToken is StandardToken {
         throw;
     }
 
-    /* Public variables of the token */
-
-    /*
-    NOTE:
-    The following variables are OPTIONAL vanities. One does not have to include them.
-    They allow one to customise the token contract & in no way influences the core functionality.
-    Some wallets/interfaces might not even bother to look at this information.
-    */
     string public name;                   //fancy name: eg Simon Bucks
     uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    string public symbol;                 //An identifier: eg SBX
-    string public version = 'H0.1';       //human 0.1 standard. Just an arbitrary versioning scheme.
+    uint yearZero;
+    address owner;
+    uint lastMonth;
 
     function CircleToken(
         uint256 _initialAmount,
         string _tokenName,
         uint8 _decimalUnits,
-        string _tokenSymbol
+        uint _yearZero
         ) {
-        balances[msg.sender] = _initialAmount;               // Give the creator all initial tokens
+        owner = msg.sender;
         totalSupply = _initialAmount;                        // Update total supply
         name = _tokenName;                                   // Set the name for display purposes
         decimals = _decimalUnits;                            // Amount of decimals for display purposes
-        symbol = _tokenSymbol;                               // Set the symbol for display purposes
+        yearZero = _yearZero;
+        balances[owner] = 1000 * ((10047 ** ((now - yearZero)/1 month))/(1000 ** ((now - yearZero)/1 month)));
+        lastMonth = ((now - yearZero)/1 month)
+    }
+
+    function payout(){
+      for(i==0; i < (((now - yearZero)/1 month) - lastMonth); i++){
+        balances[owner] += 1000 * ((10047 ** ((now - yearZero)/1 month))/(1000 ** ((now - yearZero)/1 month)))
+      }
+      lastMonth = (now - yearZero)/1 month
     }
 
     /* Approves and then calls the receiving contract */
