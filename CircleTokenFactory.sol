a7812c0859608e671c1ce3a7346aaadc18c2f245
Jared Pereira <jared@otlw.co>
2016-12-21 15:48:03 -0500

    remove code-checking capabilities of token factory

--- CircleTokenFactory.sol
+++ CircleTokenFactory.sol
@@ -8,47 +8,7 @@ contract CircleTokenFactory {
     uint yearZero;
 
     function CircleTokenFactory() {
-      //upon creation of the factory, deploy a CircleToken (parameters are meaningless) and store the bytecode provably.
-      address verifiedToken = createCircleToken(10000, "Verify Token", 3, "VTX");
-      circleByteCode = codeAt(verifiedToken);
       yearZero = now;
-    }
-
-    //verifies if a contract that has been deployed is a Human Standard Token.
-    //NOTE: This is a very expensive function, and should only be used in an eth_call. ~800k gas
-    function verifyCircleToken(address _tokenContract) returns (bool) {
-      bytes memory fetchedTokenByteCode = codeAt(_tokenContract);
-
-      if (fetchedTokenByteCode.length != circleByteCode.length) {
-        return false; //clear mismatch
-      }
-
-      //starting iterating through it if lengths match
-      for (uint i = 0; i < fetchedTokenByteCode.length; i ++) {
-        if (fetchedTokenByteCode[i] != circleByteCode[i]) {
-          return false;
-        }
-      }
-
-      return true;
-    }
-
-    //for now, keeping this internal. Ideally there should also be a live version of this that any contract can use, lib-style.
-    //retrieves the bytecode at a specific address.
-    function codeAt(address _addr) internal returns (bytes o_code) {
-      assembly {
-          // retrieve the size of the code, this needs assembly
-          let size := extcodesize(_addr)
-          // allocate output byte array - this could also be done without assembly
-          // by using o_code = new bytes(size)
-          o_code := mload(0x40)
-          // new "memory end" including padding
-          mstore(0x40, add(o_code, and(add(add(size, 0x20), 0x1f), not(0x1f))))
-          // store length in memory
-          mstore(o_code, size)
-          // actually retrieve the code, this needs assembly
-          extcodecopy(_addr, add(o_code, 0x20), 0, size)
-      }
     }
 
     function createCircleToken(string _name, uint8 _decimals, string _symbol) returns (address) {
